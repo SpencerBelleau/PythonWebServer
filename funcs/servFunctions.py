ff110923a4420144c14ab2ed35b74ec305d0d42c
Spencer Belleau <spencer.belleau@gmail.com>
2016-03-20 00:10:16 -0700

    Now uses persistent HTTP, very fun. Looks good on the memory-usage/exception front, possible there are still crashes. Also, filenames with special chars now work.

--- funcs/servFunctions.py
+++ funcs/servFunctions.py
@@ -4,8 +4,16 @@ def wrapString(string):
 def parseRequest(request):
 	#First, split the request into its logical parts
 	lines = request.split('\r\n')
+	for i in range(len(lines)):
+		lines[i] = lines[i].split(" ")
 	#next, split the first piece into COMMAND, PATH, VER
-	cpv = lines[0].split(' ')
+	cpv = lines[0]
+	#also, check if we need to keep-alive
+	#print(getParsedArgs(lines, "Connection:"))
+	if(getParsedArgs(lines, "Connection:")[1] == "keep-alive"):
+		persistent = True
+	else:
+		persistent = False
 	#print(cpv)
 	#If it's a GET
 	if(cpv[0] == "GET"):
@@ -22,12 +30,35 @@ def parseRequest(request):
 		#Data is parsed and treated as GET
 		try:
 			path = cpv[1]
-			getData = lines[11]
+			getData = lines[len(lines)-1]
 			getData = getData.replace("&", " ")
 		except:
 			path = cpv[1]
 			getData = ""
-	return (cpv[0], path, getData)
+	return (cpv[0], path, getData, persistent)
+def readReqBeta(request):
+	#First, split the request into its logical parts
+	lines = request.split('\r\n')
+	for i in range(len(lines)):
+		lines[i] = lines[i].split(" ")
+	print(getParsedArgs(lines, "Connection:"))
+def getParsedArgs(reqList, field):
+	for x in reqList:
+		if(x[0] == field):
+			return x
+	return []
+def escapeQuotes(str):
+	ret = ""
+	for char in str:
+		if(char == "\'"):
+			ret += "\\"
+			ret += char
+		elif(char == "\""):
+			ret += "\\"
+			ret += char
+		else:
+			ret += char
+	return ret
 def indexLink(str, path):
 	loc = urllib.parse.unquote_plus(path + str)
 	if(os.path.isfile(loc)):
--- server.py
+++ server.py
@@ -5,82 +5,84 @@ listen = socket(AF_INET, SOCK_STREAM)
 listen.bind(('', 80))
 listen.listen(5)
 root = os.path.abspath(os.path.dirname(__file__))
-#print(root)
 def thread(con, addr):
-	try:
-		rawRequest = con.recv(2048).decode()
-		#print(rawRequest)
-		type, req, getData = parseRequest(rawRequest)
-		#print(req)
-		#print(getData)
-		print(str(addr) + ": " + type + " " + str(req))
-		header = "HTTP/1.1 200 OK\r\n\r\n"
-		con.send(header.encode())
-		req = urllib.parse.unquote_plus(req)
-		if(req[-1] == "/"):
-			try:
-				#print("Trying HTML")
-				res = open(root + req + 'index.html', 'rb')
-				con.send(res.read())
-				res.close()
-			except:
-				try:
-					#print("Trying PHP")
-					#req = "/php/index.php"
-					t = open(root + req + "index.php", 'rb')
-					res = subprocess.getoutput("php-cgi -f " + wrapString(root + req + "index.php") + " " + getData)
-					#res = open('php/index.php', 'rb')
-					con.send(res.encode())
-					t.close()
-				except:
-					try:
-						#print("Trying Python")
-						#req = "/python/index.py"
-						t = open(root + req + "index.py", 'rb')
-						res = subprocess.getoutput("py " + wrapString(root + req + "index.py") + " " + getData)
-						#res = open('python/index.py')
-						con.send(res.encode())
-						t.close()
-					except:
-						res = ""
-						res += "<!DOCTYPE html><html><h2>Index of " + req + "</h2><ul><li><a href='..'>..</a></li>"
-						for name in (os.listdir(root + req)):
-							if(name == sys.argv[0].split('\\')[-1]):
-								continue
-							else:
-								res += "<li><a href='" + indexLink(name, (root + req)) + "'>" + indexLink(name, (root + req)) + "</a></li>"
-						res += "</ul></html>"
-						con.send(res.encode())
-		else:
-			if(req[-4:] == ".php"):
-				try:
-					res = subprocess.getoutput("php-cgi -f " + wrapString(root + req) + " " + getData)
-				except Exception as e:
-					print(e)
-					res = "N/A".encode()
-				con.send(res.encode())
-			elif(req[-3:] == ".py"):
-				try:
-					res = subprocess.getoutput("py " + wrapString(root + req) + " " + getData)
-				except Exception as e:
-					print(e)
-					res = "N/A".encode()
-				con.send(res.encode())
+	con.settimeout(10)
+	persistent = True
+	while persistent:
+		try:
+			rawRequest = con.recv(2048).decode()
+		except:
+			print("Closing port " + str(addr[1]))
+			break
+		try:
+			type, req, getData, persistent = parseRequest(rawRequest)
+			print(str(addr) + ": " + type + " " + str(req))
+			header = "HTTP/1.1 200 OK\r\n"
+			if(persistent):
+				header += "Connection: keep-alive\r\n"
 			else:
+				header += "Connection: close\r\n"
+			req = escapeQuotes(req)
+			req = urllib.parse.unquote_plus(req)
+			getData = urllib.parse.unquote_plus(getData)
+			if(req[-1] == "/"):
 				try:
-					res = open(root + req, 'rb')
-					con.send(res.read())
+					res = open(root + req + 'index.html', 'rb')
+					resp = res.read()
 					res.close()
 				except:
-					print("Bad request: " + str(req))
-					con.send("<h1 style='text-align:center'>404 lol</h1>".encode())
-					con.send("<a style='text-align:center' href='/'>Back to Index</a>".encode())
-	except Exception as e:
-		res = "An error has occurred: " + str(e)
-		con.send(res.encode())
+					try:
+						t = open(root + req + "index.php", 'rb')
+						t.close()
+						resp = subprocess.getoutput("php-cgi -f " + wrapString(root + req + "index.php") + " " + getData).encode()
+					except:
+						try:
+							t = open(root + req + "index.py", 'rb')
+							t.close()
+							resp = subprocess.getoutput("py " + wrapString(root + req + "index.py") + " " + getData).encode()
+						except:
+							resp = ""
+							resp += "<!DOCTYPE html><html><h2>Index of " + req + "</h2><ul><li><a href='..'>..</a></li>"
+							for name in (os.listdir(root + req)):
+								if(name == sys.argv[0].split('\\')[-1]):
+									continue
+								else:
+									resp += "<li><a href='" + indexLink(urllib.parse.quote_plus(name), (root + req)) + "'>" + indexLink(name, (root + req)) + "</a></li>"
+							resp += "</ul></html>"
+							resp = resp.encode()
+			else:
+				if(req[-4:] == ".php"):
+					try:
+						resp = subprocess.getoutput("php-cgi -f " + wrapString(root + req) + " " + getData).encode()
+					except Exception as e:
+						print(e)
+						resp = "N/A".encode()
+				elif(req[-3:] == ".py"):
+					try:
+						resp = subprocess.getoutput("py " + wrapString(root + req) + " " + getData).encode()
+					except Exception as e:
+						print(e)
+						resp = "N/A".encode()
+				else:
+					try:
+						res = open(root + req, 'rb')
+						resp = res.read()
+						res.close()
+					except:
+						print("Bad request: " + str(req))
+						resp = "<h1 style='text-align:center'>404 lol</h1><a style='text-align:center' href='/'>Back to Index</a>".encode()
+		except Exception as e:
+			header = "HTTP/1.1 200 OK\r\n"
+			header += "Connection: close\r\n"
+			resp = ("An error has occurred: " + str(e)).encode()
+		length = len(resp)
+		header += "content-length: " + str(length) + "\r\n"
+		header += "\r\n"
+		con.send(header.encode())
+		con.send(resp)
 	con.close()
 try:
-	myIP = json.loads(urllib.request.urlopen("http://ip.jsontest.com/").read().decode())["ip"]
+	myIP = json.loads(urllib.request.urlopen("https://api.ipify.org/?format=json").read().decode())["ip"]
 	print("Server hosted @ " + myIP + ":80")
 	while True:
 		con, addr = listen.accept()
